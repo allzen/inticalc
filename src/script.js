e4334553ba19cd79e26ae874673c2c579e2fed69
azenderowska <azenderowska@teonite.com>
2019-12-29 18:26:31 +0100

    displayed num&operators

--- src/script.js
+++ src/script.js
@@ -1,5 +1,19 @@
 let result = document.getElementById("calcResult");
 
-const getResult = (number) => {
-    return result.innerText = number;
+let number = document.getElementsByClassName("number");
+for (let i = 0; i < number.length; i++) {
+    number[i].addEventListener('click', function() {
+        console.log("number clicked: " + number[i].id);
+        result.innerText = number[i].id;
+    });
+}
+
+let operator = document.getElementsByClassName("operator");
+for(let i = 0; i < number.length; i++) {
+    operator[i].addEventListener('click', function() {
+        console.log("operator clicked: " + operator[i].id);
+        if (operator[i].id = "plus") {
+            console.log("plus");
+        }
+    })
 }
